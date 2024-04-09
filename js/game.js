4e7127e837a93c5fecf01aafff802b3ffeb8614b
Ulvi Azizli <ulviazizli96@gmail.com>
2024-04-09 15:46:48 +0200

    iteration 5

--- js/game.js
+++ js/game.js
@@ -1,3 +1,49 @@
 class Game {
-    // code to be added
+    constructor() {
+        this.startScreen = document.getElementById("game-intro");
+        this.gameScreen = document.getElementById("game-screen");
+        this.gameEndScreen = document.getElementById("game-end");
+        this.player = null;
+        this.height = 600;
+        this.width = 500;
+        this.obstacles = [];
+        this.score = 0;
+        this.lives = 3;
+        this.gameIsOver = false;
+        this.gameIntervalId;
+        this.gameLoopFrecuency = Math.round(1000/60);
+        this.player = new Player(
+            this.gameScreen,
+            200,
+            500,
+            100,
+            150,
+            "./images/car.png"
+        );
+    }
+
+    start() {
+        this.gameScreen.style.height = `${this.height}px`;
+        this.gameScreen.style.width = `${this.width}px`;
+
+        this.startScreen.style.display = "none";
+
+        this.gameScreen.style.display = "block";
+
+        this.gameIntervalId = setInterval(() => {
+            gameLoop();
+        }, this.gameLoopFrecuency)
+    }
+
+    gameLoop() {
+        this.update()
+
+        if(this.gameIsOver) {
+            clearInterval(this.gameIntervalId)
+        }
+    }
+
+    update() {
+        this.player.move();
+    } 
 }
